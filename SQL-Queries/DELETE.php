e9613b6b24cb532b01b1162483755195a07f49ac
wizarddos <wizarddos@github.com>
2020-12-10 09:39:52 +0100

    update name of variable

--- SQL-Queries/DELETE.php
+++ SQL-Queries/DELETE.php
@@ -1,11 +1,11 @@
 <?php 
-function delete($connect_file_name, $table, $Where_col, $where_val){
+function delete($connect_file_name, $table, $where_col, $where_val){
     require_once $connect_file_name;
     $connection = new mysqli($host,$db_user,$db_pass,$db_name);
     if($connection->connect_error != 0){
         return false;
     }else{
-        if($connection->query("DELETE FROM $table WHERE $Where_col = '$where_val'")){
+        if($connection->query("DELETE FROM $table WHERE $where_col = '$where_val'")){
             return true;
         }else{
             return false;
