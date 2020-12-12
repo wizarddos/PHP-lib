dde94a9e9b428cbecbfbf3f0bd339c5cfc9c9537
wizarddos <69435721+wizarddos@users.noreply.github.com>
2020-12-12 15:56:50 +0100

    fixed bug in add-article.php
    fixed bug with SQL query in add-article.php

--- account-script/add-article.php
+++ account-script/add-article.php
@@ -1,5 +1,6 @@
 <?php 
-    function addarticle($connect_file_name,$article_content, $table,$Title, $author, $addday,$addmonth,$addyear ){
+    <?php 
+    function addarticle($connect_file_name,$article_content, $table,$Title, $author ){
         require_once $connect_file_name;
         $connection = new mysqli($host, $db_user,$db_pass,$db_name);
         if($connection->connect_error != 0){
@@ -8,15 +9,11 @@
             $safe_article = htmlentities($article_content, ENT_QUOTES, "UTF-8");
             $safe_title = htmlentities($Title,ENT_QUOTES,"UTF-8");
             $safe_author = htmlentities($author,ENT_QUOTES,"UTF-8");
-            if(checkdate($addmonth,$addday,$addyear)){
-                $safe_date = $adday."-".$addmonth."-".$addyear;
-                if($connection->query("INSERT INTO $table VALUES(NULL,'$safe_title','$safe_article,'$safe_author', '$safe_date')")){
-                    return true;
-                }else{
-                    return false;
-                }
+            $safe_date = date("Y-m-d");
+            if($connection->query("INSERT INTO $table VALUES(NULL,'$safe_title','$safe_article','$safe_author', '$safe_date')")){
+                 return true;
             }else{
-                return false;
+                  return false;
             }
         }
             
